52997e5350843721c0e4d1244c6f929e637f7f88
Kevin Harvey Jr <lordty@gmail.com>
2020-08-18 11:26:31 -0500

    Added_Simulation_&_Customer_Classes

---
+++ SodaMachine/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Customer
+    {
+        //Member Variables(Has A)
+        public Wallet wallet;
+        public Backpack backpack;
+
+        //Constructor
+        public Customer()
+        {
+
+        }
+        //Methods(Can Do)
+
+    }
+}
---
+++ SodaMachine/Simulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Simulation
+    {
+        //Member Variables(Has A)
+        public SodaMachine sodaMachine;
+        public Customer customer;
+        //Constructor
+        public Simulation()
+        {
+
+        }
+        //Methods(Can Do)
+
+    }
+}
